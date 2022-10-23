9287a59aa9c6d1231b5cb7f81069e55b1479f23c
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-10-22 23:12:13 -0300

    método Adicionar Veiculo - Implementação.

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -5,6 +7,7 @@ namespace DesafioFundamentos.Models
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
         private List<string> veiculos = new List<string>();
+        private Regex regex = new Regex(@"^[a-zA-Z]{3}\-\d{4}$");
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
@@ -17,6 +20,15 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine().Trim();
+
+            while (!regex.IsMatch(placa))
+            {
+                Console.WriteLine("Digite uma placa de veículo válida para poder estacionar:");
+                placa = Console.ReadLine().Trim();
+            }
+
+            veiculos.Add(placa);
         }
 
         public void RemoverVeiculo()
@@ -25,18 +37,19 @@ namespace DesafioFundamentos.Models
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
-            string placa = "";
+            string placa = Console.ReadLine().Trim();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                int quantidadeHoras = int.Parse(Console.ReadLine().Trim());
 
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
                 int horas = 0;
-                decimal valorTotal = 0; 
+                decimal valorTotal = 0;
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
